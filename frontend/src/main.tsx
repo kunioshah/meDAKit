bfc01ce601619504432706ecd1edd4fe8f6d6edf
kunioshah <kunioshah@gmail.com>
2026-04-04 23:55:04 -0700

    https + vite config changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import App from "./app/App.tsx";
 import MobilePage from "./app/MobilePage.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/mobile" element={<MobilePage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
